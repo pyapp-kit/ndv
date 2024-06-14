1714b5e886b6d3773da18e1f86e5f51579e7ef4c
Talley Lambert <talley.lambert@gmail.com>
2024-06-14 17:44:53 -0400

    remove fixture... causing leaks

--- tests/test_nd_viewer.py
+++ tests/test_nd_viewer.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 
 import os
 import sys
-from typing import TYPE_CHECKING, Any, Iterator
+from typing import TYPE_CHECKING, Any
 
-import dask.array as da
 import numpy as np
 import pytest
 from qtpy.QtCore import QEvent, QPointF, Qt
@@ -22,51 +21,32 @@ def allow_linux_widget_leaks(func: Any) -> Any:
     return func
 
 
-def make_lazy_array(shape: tuple[int, ...]) -> da.Array:
-    rest_shape = shape[:-2]
-    frame_shape = shape[-2:]
-
-    def _dask_block(block_id: tuple[int, int, int, int, int]) -> np.ndarray | None:
-        if isinstance(block_id, np.ndarray):
-            return None
-        size = (1,) * len(rest_shape) + frame_shape
-        return np.random.randint(0, 255, size=size, dtype=np.uint8)
-
-    chunks = [(1,) * x for x in rest_shape] + [(x,) for x in frame_shape]
-    return da.map_blocks(_dask_block, chunks=chunks, dtype=np.uint8)  # type: ignore
-
-
 BACKENDS = ["vispy"]
 # avoid pygfx backend on linux CI
 if not os.getenv("CI") or sys.platform == "darwin":
     BACKENDS.append("pygfx")
 
 
-@pytest.fixture(params=BACKENDS)
-def viewer(
-    qtbot: QtBot, request: pytest.FixtureRequest, monkeypatch: pytest.MonkeyPatch
-) -> Iterator[NDViewer]:
-    monkeypatch.setenv("NDV_CANVAS_BACKEND", request.param)
-    v = NDViewer()
+@allow_linux_widget_leaks
+@pytest.mark.filterwarnings("ignore:This version of pygfx does not yet")
+@pytest.mark.parametrize("backend", BACKENDS)
+def test_ndviewer(qtbot: QtBot, backend: str, monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setenv("NDV_CANVAS_BACKEND", backend)
+    dask_arr = np.ones((4, 3, 2, 32, 32), dtype=np.uint8)
+    v = NDViewer(dask_arr)
     qtbot.addWidget(v)
-    yield v
+    v.show()
+    qtbot.waitUntil(v._is_idle, timeout=1000)
+    v.set_ndim(3)
+    v.set_channel_mode("composite")
+    v.set_current_index({0: 2, 1: 1, 2: 1})
+
     # wait until there are no running jobs, because the callbacks
     # in the futures hold a strong reference to the viewer
     qtbot.waitUntil(v._is_idle, timeout=3000)
 
 
-@allow_linux_widget_leaks
-def test_ndviewer(qtbot: QtBot, viewer: NDViewer) -> None:
-    data = np.empty((4, 3, 2, 32, 32), dtype=np.uint8)
-    viewer.set_data(data)
-    viewer.show()
-    qtbot.waitUntil(viewer._is_idle, timeout=1000)
-    viewer.set_ndim(3)
-    viewer.set_channel_mode("composite")
-    viewer.set_current_index({0: 2, 1: 0, 2: 1})
-
-
-@pytest.mark.allow_leaks
+# not testing pygfx yet...
 def test_hover_info(qtbot: QtBot) -> None:
     data = np.ones((4, 3, 32, 32), dtype=np.uint8)
     viewer = NDViewer(data)
