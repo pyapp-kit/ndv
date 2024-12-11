536140bddebd663d765cd0073d2041332fe62255
Talley Lambert <talley.lambert@gmail.com>
2024-12-10 19:14:33 -0500

    tile again

--- mvc.py
+++ mvc.py
@@ -1,12 +1,14 @@
 """Example usage of new mvc pattern."""
 
+import numpy as np
+
 from ndv import data, run_app
 from ndv.controller import ViewerController
 
 viewer = ViewerController()
 
 img_data = data.cells3d()
-# img_data = np.tile(img_data, (2, 1, 3, 4))
+img_data = np.tile(img_data, (2, 1, 3, 4))
 viewer.data = img_data
 viewer.show()
 viewer.model.current_index.update({0: 32})
