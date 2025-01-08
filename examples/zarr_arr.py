c51990bdf7e2886624e683f767afcd175c030a4a
Talley Lambert <talley.lambert@gmail.com>
2025-01-08 11:35:49 -0500

    feat: support GPU-calculated luts in vispy (#77)
    * feat: support gpu clims
    
    * Update _utils.py
    
    Co-authored-by: Gabriel Selzer <gjselzer@wisc.edu>
    
    * fix downcast
    
    * style(pre-commit.ci): auto fixes [...]
    
    * fix for int16
    
    * style(pre-commit.ci): auto fixes [...]
    
    ---------
    
    Co-authored-by: Gabriel Selzer <gjselzer@wisc.edu>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- examples/zarr_arr.py
+++ examples/zarr_arr.py
@@ -12,4 +12,4 @@ except ImportError:
 URL = "https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/tczyx.ome.zarr"
 zarr_arr = zarr.open(URL, mode="r")
 
-ndv.imshow(zarr_arr["s0"])
+ndv.imshow(zarr_arr["s0"].astype("uint16"))
