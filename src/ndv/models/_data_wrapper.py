5406cea490c75ddc083984850346dbf272c14519
Talley Lambert <talley.lambert@gmail.com>
2025-01-27 08:12:34 -0500

    simplify subclasses

--- src/ndv/models/_data_wrapper.py
+++ src/ndv/models/_data_wrapper.py
@@ -58,6 +58,12 @@ class DataWrapper(Generic[ArrayT], ABC):
     supports and isel methods. Ensure that your class is imported before the
     `DataWrapper.create` method is called, and it will be automatically detected and
     used to wrap your data.
+
+    This base class provides basic support for numpy-like array types.  If the data
+    supports __getitem__ and shape attributes, it will work.  If the data does not
+    support __getitem__, the subclass MUST implement the `isel` method.  If the data
+    does not have a `shape` attribute, the subclass MUST implement the `dims` and
+    `coords` properties.
     """
 
     # Order in which subclasses are checked for support.
@@ -70,16 +76,30 @@ class DataWrapper(Generic[ArrayT], ABC):
     COMMON_Z_AXIS_NAMES: ClassVar[Container[str]] = ("z", "depth", "focus")
 
     # Maximum dimension size consider when guessing the channel axis
-    MAX_CHANNELS = 16
+    MAX_CHANNELS: ClassVar[int] = 16
 
     def __init__(self, data: ArrayT) -> None:
         self._data = data
+        if not hasattr(self._data, "__getitem__") and "isel" not in type(self).__dict__:
+            raise NotImplementedError(
+                "DataWrapper subclass MUST implement `isel` method if data does not "
+                "support __getitem__."
+            )
+
+        has_shape = hasattr(self._data, "shape") and isinstance(self._data.shape, tuple)
+        has_methods = "dims" in type(self).__dict__ and "coords" in type(self).__dict__
+        if not has_shape and not has_methods:
+            raise NotImplementedError(
+                "DataWrapper subclass MUST implement `dims` and `coords` properties"
+                " if data does not have a `shape` attribute or if the shape is not "
+                "a tuple."
+            )
 
     # ----------------------------- Mandatory methods -----------------------------
 
     @classmethod
     @abstractmethod
-    def supports(cls, obj: Any) -> bool:
+    def supports(cls, obj: Any) -> TypeGuard[Any]:
         """Return True if this wrapper can handle the given object.
 
         Any exceptions raised by this method will be suppressed, so it is safe to
@@ -87,21 +107,35 @@ class DataWrapper(Generic[ArrayT], ABC):
         """
 
     @property
-    @abstractmethod
     def dims(self) -> tuple[Hashable, ...]:
         """Return the dimension labels for the data."""
+        # type ignore is asserted in the __init__ method
+        return tuple(range(len(self._data.shape)))  # type: ignore [attr-defined]
 
     @property
-    @abstractmethod
     def coords(self) -> Mapping[Hashable, Sequence]:
         """Return the coordinates for the data."""
+        dims = self.dims
+        # type ignore is asserted in the __init__ method
+        return {i: range(s) for i, s in zip(dims, self._data.shape)}  # type: ignore [attr-defined]
 
-    @abstractmethod
     def isel(self, index: Mapping[int, int | slice]) -> np.ndarray:
         """Return a slice of the data as a numpy array.
 
         `index` will look like (e.g.) `{0: slice(0, 10), 1: 5}`.
+        The default implementation converts the index to a tuple of the same length as
+        the self.dims, populating missing keys with `slice(None)`, and then slices the
+        data array using __getitem__.
         """
+        idx = tuple(index.get(k, slice(None)) for k in range(len(self.dims)))
+        # this type ignore is asserted in the __init__ method
+        # if the data does not support __getitem__, then the DataWrapper subclass will
+        # fail to initialize
+        return self._asarray(self._data[idx])  # type: ignore [index]
+
+    def _asarray(self, data: Any) -> np.ndarray:
+        """Convert data to a numpy array."""
+        return np.asarray(data)
 
     def save_as_zarr(self, path: str) -> None:
         raise NotImplementedError("Saving as zarr is not supported for this data type")
@@ -260,6 +294,77 @@ class DataWrapper(Generic[ArrayT], ABC):
 ##########################
 
 
+class ArrayLikeWrapper(DataWrapper[NPArrayLike]):
+    """Wrapper for numpy duck array-like objects.
+
+    The base class is suitable for any object that supports __getitem__ and shape.
+    So we just need to implement supports to define the type of object we are wrapping.
+    """
+
+    PRIORITY = 100
+
+    @classmethod
+    def supports(cls, obj: Any) -> TypeGuard[NPArrayLike]:
+        if (
+            (
+                isinstance(obj, np.ndarray)
+                or hasattr(obj, "__array_function__")
+                or hasattr(obj, "__array_namespace__")
+                or hasattr(obj, "__array__")
+            )
+            and hasattr(obj, "__getitem__")
+            and hasattr(obj, "shape")
+        ):
+            return True
+        return False
+
+
+class DaskWrapper(DataWrapper["da.Array"]):
+    """Wrapper for dask array objects."""
+
+    @classmethod
+    def supports(cls, obj: Any) -> TypeGuard[da.Array]:
+        if (da := sys.modules.get("dask.array")) and isinstance(obj, da.Array):
+            return True
+        return False
+
+    def _asarray(self, data: da.Array) -> np.ndarray:
+        return np.asarray(data.compute())
+
+    def save_as_zarr(self, path: str) -> None:
+        self._data.to_zarr(url=path)
+
+
+class SparseArrayWrapper(DataWrapper["sparse.Array"]):
+    PRIORITY = 50
+
+    @classmethod
+    def supports(cls, obj: Any) -> TypeGuard[sparse.COO]:
+        if (sparse := sys.modules.get("sparse")) and isinstance(obj, sparse.COO):
+            return True
+        return False
+
+    def _asarray(self, data: sparse.COO) -> np.ndarray:
+        return np.asarray(data.todense())
+
+
+class CLArrayWrapper(DataWrapper["cl_array.Array"]):
+    """Wrapper for pyopencl array objects."""
+
+    PRIORITY = 50
+
+    @classmethod
+    def supports(cls, obj: Any) -> TypeGuard[cl_array.Array]:
+        if (cl_array := sys.modules.get("pyopencl.array")) and isinstance(
+            obj, cl_array.Array
+        ):
+            return True
+        return False
+
+    def _asarray(self, data: cl_array.Array) -> np.ndarray:
+        return np.asarray(data.get())
+
+
 class XarrayWrapper(DataWrapper["xr.DataArray"]):
     """Wrapper for xarray DataArray objects."""
 
@@ -272,10 +377,6 @@ class XarrayWrapper(DataWrapper["xr.DataArray"]):
     def coords(self) -> Mapping[Hashable, Sequence]:
         """Return the coordinates for the data."""
         return self.data.coords  # type: ignore [no-any-return]
-
-    def isel(self, indexers: Mapping[int, int | slice]) -> np.ndarray:
-        idx = tuple(indexers.get(k, slice(None)) for k in range(len(self.data.shape)))
-        return np.asarray(self.data[idx])
 
     @classmethod
     def supports(cls, obj: Any) -> TypeGuard[xr.DataArray]:
@@ -346,91 +447,6 @@ class TensorstoreWrapper(DataWrapper["ts.TensorStore"]):
         return False
 
 
-class ArrayLikeWrapper(DataWrapper[NPArrayLike]):
-    """Wrapper for numpy duck array-like objects."""
-
-    PRIORITY = 100
-
-    @property
-    def dims(self) -> tuple[Hashable, ...]:
-        """Return the dimension labels for the data."""
-        return tuple(range(len(self.data.shape)))
-
-    @property
-    def coords(self) -> Mapping[Hashable, Sequence]:
-        """Return the coordinates for the data."""
-        return {i: range(s) for i, s in enumerate(self.data.shape)}
-
-    def isel(self, indexers: Mapping[int, int | slice]) -> np.ndarray:
-        idx = tuple(indexers.get(k, slice(None)) for k in range(len(self.data.shape)))
-        return self._asarray(self.data[idx])
-
-    @classmethod
-    def supports(cls, obj: Any) -> TypeGuard[SupportsIndexing]:
-        if (
-            (
-                isinstance(obj, np.ndarray)
-                or hasattr(obj, "__array_function__")
-                or hasattr(obj, "__array_namespace__")
-                or hasattr(obj, "__array__")
-            )
-            and hasattr(obj, "__getitem__")
-            and hasattr(obj, "shape")
-        ):
-            return True
-        return False
-
-    def _asarray(self, data: npt.ArrayLike) -> np.ndarray:
-        """Convert data to a numpy array."""
-        return np.asarray(data)
-
-
-class SparseArrayWrapper(ArrayLikeWrapper["sparse.Array"]):
-    PRIORITY = 50
-
-    @classmethod
-    def supports(cls, obj: Any) -> TypeGuard[sparse.COO]:
-        if (sparse := sys.modules.get("sparse")) and isinstance(obj, sparse.COO):
-            return True
-        return False
-
-    def _asarray(self, data: sparse.COO) -> np.ndarray:
-        return np.asarray(data.todense())
-
-
-class CLArrayWrapper(ArrayLikeWrapper["cl_array.Array"]):
-    """Wrapper for pyopencl array objects."""
-
-    PRIORITY = 50
-
-    @classmethod
-    def supports(cls, obj: Any) -> TypeGuard[cl_array.Array]:
-        if (cl_array := sys.modules.get("pyopencl.array")) and isinstance(
-            obj, cl_array.Array
-        ):
-            return True
-        return False
-
-    def _asarray(self, data: cl_array.Array) -> np.ndarray:
-        return np.asarray(data.get())
-
-
-class DaskWrapper(ArrayLikeWrapper["da.Array"]):
-    """Wrapper for dask array objects."""
-
-    @classmethod
-    def supports(cls, obj: Any) -> TypeGuard[da.Array]:
-        if (da := sys.modules.get("dask.array")) and isinstance(obj, da.Array):
-            return True
-        return False
-
-    def _asarray(self, data: da.Array) -> np.ndarray:
-        return np.asarray(data.compute())
-
-    def save_as_zarr(self, path: str) -> None:
-        self._data.to_zarr(url=path)
-
-
 class TorchTensorWrapper(DataWrapper["torch.Tensor"]):
     """Wrapper for torch tensor objects."""
 
@@ -447,10 +463,6 @@ class TorchTensorWrapper(DataWrapper["torch.Tensor"]):
             names = self.data.names
             return tuple((i if name is None else name) for i, name in enumerate(names))
         return tuple(range(len(self.data.shape)))
-
-    def isel(self, indexers: Mapping[int, int | slice]) -> np.ndarray:
-        idx = tuple(indexers.get(k, slice(None)) for k in range(len(self.data.shape)))
-        return np.asarray(self.data[idx])
 
     @classmethod
     def supports(cls, obj: Any) -> TypeGuard[torch.Tensor]:
