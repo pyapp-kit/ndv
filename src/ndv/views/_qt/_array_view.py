2c82b9d429b405b72f2e3be0a06794b809ce603c
Gabriel Selzer <gjselzer@wisc.edu>
2025-02-13 13:36:53 -0600

    WIP: Remove gross closures

--- src/ndv/views/_qt/_array_view.py
+++ src/ndv/views/_qt/_array_view.py
@@ -5,6 +5,7 @@ from contextlib import suppress
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, cast
 
+import psygnal
 from qtpy.QtCore import QSize, Qt, Signal
 from qtpy.QtGui import QMovie
 from qtpy.QtWidgets import (
@@ -177,12 +178,15 @@ class _QLUTWidget(QWidget):
 
 
 class QLutView(LutView):
+    # NB: In practice this will be a ChannelKey but Unions not allowed here.
+    histogramRequested = psygnal.Signal(object)
+
     def __init__(self, channel: ChannelKey = None) -> None:
         super().__init__()
         self._qwidget = _QLUTWidget()
         self._channel = channel
-        self._histogram: QWidget | None = None
         # TODO: use emit_fast
+        self._qwidget.histogram_btn.toggled.connect(self._on_q_histogram_toggled)
         self._qwidget.visible.toggled.connect(self._on_q_visibility_changed)
         self._qwidget.cmap.currentColormapChanged.connect(self._on_q_cmap_changed)
         self._qwidget.clims.valueChanged.connect(self._on_q_clims_changed)
@@ -236,6 +240,16 @@ class QLutView(LutView):
             else:
                 clims = self._qwidget.clims.value()
                 self._model.clims = ClimsManual(min=clims[0], max=clims[1])
+
+    def _on_q_histogram_toggled(self, toggled: bool) -> None:
+        container = self._qwidget._histogram_container
+        if count := container.count():
+            for i in range(count):
+                item = container.itemAt(i).widget()
+                if isinstance(item, QWidget):
+                    item.setVisible(toggled)
+        elif toggled:
+            self.histogramRequested.emit(self._channel)
 
 
 class _QDimsSliders(QWidget):
@@ -370,10 +384,6 @@ class _QArrayViewer(QWidget):
         set_range_icon = QIconifyIcon("fluent:full-screen-maximize-24-filled")
         self.set_range_btn = QPushButton(set_range_icon, "", self)
 
-        # # button to add a histogram
-        # add_histogram_icon = QIconifyIcon("foundation:graph-bar")
-        # self.histogram_btn = QPushButton(add_histogram_icon, "", self)
-
         self.luts = _UpCollapsible(
             "LUTs",
             parent=self,
@@ -389,7 +399,6 @@ class _QArrayViewer(QWidget):
 
         self._btn_layout.addWidget(self.channel_mode_combo)
         self._btn_layout.addWidget(self.ndims_btn)
-        # self._btn_layout.addWidget(self.histogram_btn)
         self._btn_layout.addWidget(self.set_range_btn)
         # self._btns.addWidget(self._add_roi_btn)
 
@@ -456,14 +465,7 @@ class QtArrayView(ArrayView):
         view = QLutView(channel)
         self._luts[channel] = view
 
-        def _on_histogram_requested(toggled: bool) -> None:
-            if view._histogram:
-                view._histogram.setVisible(toggled)
-            else:
-                self.histogramRequested.emit(view._channel)
-
-        # TODO: This ugly
-        view._qwidget.histogram_btn.toggled.connect(_on_histogram_requested)
+        view.histogramRequested.connect(self.histogramRequested)
         self._qwidget.luts.addWidget(view.frontend_widget())
         return view
 
@@ -485,20 +487,12 @@ class QtArrayView(ArrayView):
 
     def add_histogram(self, channel: ChannelKey, widget: QWidget) -> None:
         if lut := self._luts.get(channel, None):
-            # FIXME: Yuck
+            # # FIXME: Yuck
             lut._qwidget.resize(
                 QSize(lut._qwidget.width(), lut._qwidget.height() + 100)
             )
             widget.resize(QSize(lut._qwidget.width(), 100))
             lut._qwidget._histogram_container.addWidget(widget)
-            lut._histogram = widget
-
-        # self._qwidget.luts.childAt
-        # if hasattr(self, "_hist"):
-        #     raise RuntimeError("Only one histogram can be added at a time")
-        # self._hist = widget
-        # self._qwidget.splitter.addWidget(widget)
-        # self._qwidget.splitter.setSizes([self._qwidget.height() - 100, 100])
 
     def remove_histogram(self, widget: QWidget) -> None:
         widget.setParent(None)
