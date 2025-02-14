f9586e059cb2cfaae957dccc141b55cd537a1e8a
Gabriel Selzer <gjselzer@wisc.edu>
2025-02-14 16:58:05 -0600

    Revert changes to examples

--- examples/numpy_arr.py
+++ examples/numpy_arr.py
@@ -6,12 +6,7 @@
 # ///
 from __future__ import annotations
 
-import os
-
 import ndv
-
-# os.environ["NDV_CANVAS_BACKEND"] = "pygfx"
-os.environ["NDV_GUI_FRONTEND"] = "wx"
 
 try:
     img = ndv.data.cells3d()
