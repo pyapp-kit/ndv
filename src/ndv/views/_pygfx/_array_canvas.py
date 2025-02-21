2ebbf5db6019c42e54bcb5f51750a140f860c44f
Gabriel Selzer <gjselzer@wisc.edu>
2025-02-21 11:57:23 -0600

    Clean up pygfx RGB changeset

--- src/ndv/views/_pygfx/_array_canvas.py
+++ src/ndv/views/_pygfx/_array_canvas.py
@@ -55,14 +55,17 @@ class PyGFXImageHandle(ImageHandle):
         return self._grid.data  # type: ignore [no-any-return]
 
     def set_data(self, data: np.ndarray) -> None:
+        # If dimensions are unchanged, reuse the buffer
         if data.shape == self._grid.data.shape:
             self._grid.data[:] = data
             self._grid.update_range((0, 0, 0), self._grid.size)
+        # Otherwise, the size (and maybe number of dimensions) changed
+        # - we need a new buffer
         else:
             self._grid = pygfx.Texture(data, dim=2)
             self._image.geometry = pygfx.Geometry(grid=self._grid)
-            if data.ndim == 3:
-                self._material.map = None
+            # RGB images (i.e. 3D datasets) cannot have a colormap
+            self._material.map = None if data.ndim == 3 else self._cmap.to_pygfx()
 
     def visible(self) -> bool:
         return bool(self._image.visible)
@@ -98,10 +101,12 @@ class PyGFXImageHandle(ImageHandle):
         return self._cmap
 
     def set_colormap(self, cmap: _cmap.Colormap) -> None:
-        self._cmap = cmap
         # RGB (i.e. 3D) images should not have a colormap
         if self.data().ndim != 3:
-            self._material.map = cmap.to_pygfx()
+            warnings.warn("Cannot set colormap on an RGB image", stacklevel=2)
+            return
+        self._cmap = cmap
+        self._material.map = cmap.to_pygfx()
         self._render()
 
     def start_move(self, pos: Sequence[float]) -> None:
