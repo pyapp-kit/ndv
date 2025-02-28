371a87864b03cc3f43d9cb969ee4558766a9b02d
Gabriel Selzer <gjselzer@wisc.edu>
2025-02-28 12:52:07 -0600

    fix: Properly remove wx buttons labels (#145)
    * WIP: Remove label on Windows
    
    * Remove size get/set

--- src/ndv/views/_wx/_array_view.py
+++ src/ndv/views/_wx/_array_view.py
@@ -62,6 +62,8 @@ def _add_icon(btn: wx.AnyButton, icon: str) -> None:
 
     icon_path = svg_path(icon)
     bitmap = wx.BitmapBundle.FromSVGFile(str(icon_path), wx.Size(16, 16))
+    # Note - label must be cleared first so bitmap is center-aligned
+    btn.SetLabel("")
     btn.SetBitmapLabel(bitmap)
 
 
