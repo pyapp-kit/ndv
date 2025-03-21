1525be34404cb6ddd4620db97f5a18ba4d9d8c37
Gabriel Selzer <gjselzer@wisc.edu>
2025-03-20 23:44:03 -0400

    Flesh out vispy histogram tests

--- tests/views/_vispy/test_histogram.py
+++ tests/views/_vispy/test_histogram.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
+import numpy as np
 import pytest
 from pytest import fixture
 
-from ndv._types import MouseButton, MouseMoveEvent, MousePressEvent, MouseReleaseEvent
+from ndv._types import (
+    CursorType,
+    MouseButton,
+    MouseMoveEvent,
+    MousePressEvent,
+    MouseReleaseEvent,
+)
 from ndv.models._lut_model import ClimsManual, LUTModel
 from ndv.views._vispy._histogram import VispyHistogramCanvas
 
@@ -20,25 +27,54 @@ def model() -> LUTModel:
 @fixture
 def histogram() -> VispyHistogramCanvas:
     canvas = VispyHistogramCanvas()
-    canvas.set_range(x=(0, 100), y=(0, 1))
+    canvas.set_range(x=(0, 10), y=(0, 1))
     return canvas
 
 
 @pytest.mark.usefixtures("any_app")
 def test_interaction(model: LUTModel, histogram: VispyHistogramCanvas) -> None:
     """Checks basic histogram functionality."""
-    # Setup
     histogram.model = model
-    left, right = 20, 80
+    left, right = 0, 10
     histogram.set_clims((left, right))
 
     def world_to_canvas(x: float, y: float) -> tuple[float, float]:
         return tuple(histogram.node_tform.imap((x, y))[:2])
 
+    # Test cursors
+    x, y = world_to_canvas((left + right) / 2, 0.5)
+    assert (
+        histogram.get_cursor(MouseMoveEvent(x=x, y=y, btn=MouseButton.NONE))
+        == CursorType.V_ARROW
+    )
+    x, y = world_to_canvas(left, 0)
+    assert (
+        histogram.get_cursor(MouseMoveEvent(x=x, y=y, btn=MouseButton.NONE))
+        == CursorType.H_ARROW
+    )
+    x, y = world_to_canvas(right, 0)
+    assert (
+        histogram.get_cursor(MouseMoveEvent(x=x, y=y, btn=MouseButton.NONE))
+        == CursorType.H_ARROW
+    )
+
+    # Select and move gamma
+    x, y = world_to_canvas((left + right) / 2, 0.5)
+    histogram.on_mouse_press(MousePressEvent(x=x, y=y, btn=MouseButton.LEFT))
+    x, y = world_to_canvas((left + right) / 2, 0.75)
+    histogram.on_mouse_move(MouseMoveEvent(x=x, y=y, btn=MouseButton.LEFT))
+    histogram.on_mouse_release(MouseReleaseEvent(x=x, y=y, btn=MouseButton.LEFT))
+    assert model.gamma == -np.log2(0.75)
+
+    # Double clicking gamma resets to 1.
+    x, y = world_to_canvas((left + right) / 2, 0.75)
+    histogram.on_mouse_double_press(MousePressEvent(x=x, y=y, btn=MouseButton.LEFT))
+    assert model.gamma == 1
+
     # Select and move the left clim
     x, y = world_to_canvas(left, 0)
     histogram.on_mouse_press(MousePressEvent(x=x, y=y, btn=MouseButton.LEFT))
-    left = 30
+    left = 1
     x, y = world_to_canvas(left, 0)
     histogram.on_mouse_move(MouseMoveEvent(x=x, y=y, btn=MouseButton.LEFT))
     histogram.on_mouse_release(MouseReleaseEvent(x=x, y=y, btn=MouseButton.LEFT))
@@ -47,7 +83,25 @@ def test_interaction(model: LUTModel, histogram: VispyHistogramCanvas) -> None:
     # Select and move the right clim
     x, y = world_to_canvas(right, 0)
     histogram.on_mouse_press(MousePressEvent(x=x, y=y, btn=MouseButton.LEFT))
-    right = 50
+    right = 9
+    x, y = world_to_canvas(right, 0)
+    histogram.on_mouse_move(MouseMoveEvent(x=x, y=y, btn=MouseButton.LEFT))
+    histogram.on_mouse_release(MouseReleaseEvent(x=x, y=y, btn=MouseButton.LEFT))
+    assert model.clims == ClimsManual(min=left, max=right)
+
+    # Ensure the right clim cannot move beyond the left clim
+    x, y = world_to_canvas(right, 0)
+    histogram.on_mouse_press(MousePressEvent(x=x, y=y, btn=MouseButton.LEFT))
+    right = 0
+    x, y = world_to_canvas(right, 0)
+    histogram.on_mouse_move(MouseMoveEvent(x=x, y=y, btn=MouseButton.LEFT))
+    histogram.on_mouse_release(MouseReleaseEvent(x=x, y=y, btn=MouseButton.LEFT))
+    assert model.clims == ClimsManual(min=left, max=left)
+
+    # Ensure right clim is chosen when overlapping
+    x, y = world_to_canvas(left, 0)
+    histogram.on_mouse_press(MousePressEvent(x=x, y=y, btn=MouseButton.LEFT))
+    right = 9
     x, y = world_to_canvas(right, 0)
     histogram.on_mouse_move(MouseMoveEvent(x=x, y=y, btn=MouseButton.LEFT))
     histogram.on_mouse_release(MouseReleaseEvent(x=x, y=y, btn=MouseButton.LEFT))
