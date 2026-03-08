0fcf1763809db2394e8fcf23a0f04acdfdc426c2
Talley Lambert <talley.lambert@gmail.com>
2026-03-08 15:15:49 -0400

    more robust test

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -648,24 +648,22 @@ def test_data_replacement_with_stale_index() -> None:
     ctrl._join()
 
 
-@pytest.mark.usefixtures("any_app")
+@no_type_check
+@_patch_views
 def test_remove_lut_view_with_non_gui_view() -> None:
     """remove_lut_view should handle non-GUI LUTViews (e.g. ImageHandle).
 
     See https://github.com/pyapp-kit/ndv/issues/138
-    For now, ArrayViews simply ignore attempts to remove LUT views that aren't found.
-    (just a guardrail... not a deeper architectural fix.)
     """
-    viewer = ArrayViewer()
-    viewer.show()
-    viewer.data = np.random.randint(0, 255, size=(10, 10), dtype="uint8")
-    _app.process_events()
+    ctrl = ArrayViewer()
+    ctrl._async = False
+    ctrl.data = np.random.randint(0, 255, size=(10, 10), dtype="uint8")
 
-    lut_ctrl = next(iter(viewer._lut_controllers.values()))
+    lut_ctrl = next(iter(ctrl._lut_controllers.values()))
     # lut_views contains both the GUI LUTView and the ImageHandle
     for view in lut_ctrl.lut_views:
         # This should not raise, even for non-GUI views like ImageHandle
-        viewer._view.remove_lut_view(view)
+        ctrl._view.remove_lut_view(view)
 
 
 @no_type_check
